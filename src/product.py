3435f365de13a136d41326f0421d88ad187e8e6a
Kuprin Aleksandr <freeman8901@yandex.ru>
2025-11-15 15:11:17 +0500

    Выполнение основного задания

---
+++ src/product.py
@@ -0,0 +1,14 @@
+class Product:
+    """
+    Класс продуктов и их цены и количества
+    """
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
