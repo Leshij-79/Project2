45f82ab42dc35596ab9522eb540c0f23e85fbb13
Kuprin Aleksandr <freeman8901@yandex.ru>
2025-11-20 23:06:30 +0500

    Выполнение пунктов 1-2 задания 14.2

--- src/category.py
+++ src/category.py
@@ -15,7 +15,25 @@ class Category:
     def __init__(self, name: str, description: str, products: list[Product]):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    @property
+    def products(self) -> list:
+        return_str = ""
+        item_int = 1
+        for item in self.__products:
+            if item_int < len(self.__products):
+                return_str += f'{item.name}, {item.price} руб.Остаток: {item.quantity} шт.\n'
+            else:
+                return_str += f'{item.name}, {item.price} руб.Остаток: {item.quantity} шт.'
+            item_int += 1
+        return return_str
+
+
+    def add_product(self, product: Product):
+        Category.product_count += 1
+        self.__products.append(product)
